2d8c7c5a5948ec9d2bfe9814cb5cd6861783df65
BHObserver <BHObserver0@gmail.com>
2024-10-09 12:35:04 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.scss';
-import Header from './components/Header';
+import Header from './components/header/Header';
+import UserProfileForm from './components/user-profile-form/UserProfileForm';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <UserProfileForm />
     </div>
   );
 }
