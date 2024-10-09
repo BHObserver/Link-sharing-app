a0277273a89a718ef0f07552e5970eefc66a7214
BHObserver <BHObserver0@gmail.com>
2024-10-09 15:16:28 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Header from './components/header/Header';
 import UserProfileForm from './components/user-profile-form/UserProfileForm';
+import PreviewPage from './components/preview/PreviewPage';
 
 function App() {
+  const [isPreviewVisible, setIsPreviewVisible] = useState(false);
+  const [userProfile, setUserProfile] = useState({
+    name: '',
+    email: '',
+    profilePicture: '',
+    links: [],
+  });
+
+  const handleToggleView = () => {
+    setIsPreviewVisible(!isPreviewVisible);
+  };
+
   return (
     <div className="App">
       <Header />
-      <UserProfileForm />
+      <button className="toggle-button" onClick={handleToggleView}>
+        {isPreviewVisible ? 'Edit Profile' : 'View Preview'}
+      </button>
+      {isPreviewVisible ? (
+        <PreviewPage userProfile={userProfile} />
+      ) : (
+        <UserProfileForm setUserProfile={setUserProfile} />
+      )}
     </div>
   );
 }
