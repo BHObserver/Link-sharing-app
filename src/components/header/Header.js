3642ff78ce90b3931b592f82e599be7eb81ec17f
BHObserver <BHObserver0@gmail.com>
2024-10-11 15:48:50 +0600

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './Header.scss';
+import Logo from '../../assets/logo-head.png'; // Import the logo image correctly
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__logo">devlinks</div>
+      <div className="header__logo">
+        <img src={Logo} alt="Logo" /> {/* Correctly render the logo */}
+        devlinks
+      </div>
       <nav className="header__nav">
         {/* Update anchor tags to Link components */}
         <Link to="/links" className="header__link">Links</Link>
         <Link to="/" className="header__link">Profile Details</Link>
-        {/* If you have a separate preview page, you can add the Link to it here */}
-        <Link to="/preview" className="header__button">Preview</Link>
+        <Link to="/preview" className="header__button">Preview</Link> {/* Button-style link */}
       </nav>
     </header>
   );
